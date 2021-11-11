5425d4d937aa0a995348aaa38edc13f495a72650
jmedinr <juandiego619@gmail.com>
2021-11-11 17:12:59 -0500

    Cambios 11-11-2021

--- appciudadescolombia/app/src/main/java/com/example/appciudadescolombia/DataResource.kt
+++ appciudadescolombia/app/src/main/java/com/example/appciudadescolombia/DataResource.kt
@@ -3,7 +3,8 @@ package com.example.appciudadescolombia
 class DataResource {
     fun loadCities(): List<CityInformation> {
         return listOf(
-            CityInformation(R.string.city1, R.drawable.barranquilla,R.string.barranquilla_text)
+            CityInformation(R.string.city1, R.drawable.barranquilla,R.string.barranquilla_text),
+            CityInformation(R.string.city2, R.drawable.armenia,R.string.armenia_text)
         )
     }
 }
