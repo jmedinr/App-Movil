571081b1ab9fc10d8158a8af29919c11a8f47927
jmedinr <juandiego619@gmail.com>
2021-11-20 09:36:30 -0500

    Cambios 20-11-2021

--- appciudadescolombia/app/app/src/main/java/com/example/cityrv/CityInformation.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cityrv
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class CityInformation(@StringRes val cityNameResourceId: Int, @DrawableRes val cityImageResourceId: Int)
