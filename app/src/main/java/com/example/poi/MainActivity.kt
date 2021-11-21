ea310e828fd6cfc1f43767deddf25e5d42c1effe
HacheCe <hectoremilcortes@gmail.com>
2021-11-21 14:23:46 -0500

    Continuación versión con Recycler view - Inicio pruebas

--- app/src/main/java/com/example/poi/MainActivity.kt
+++ app/src/main/java/com/example/poi/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import org.json.JSONArray
 import org.json.JSONException
+import java.io.IOException
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -26,40 +27,63 @@ class MainActivity : AppCompatActivity() {
     private fun setupRecyclerView(){
         mCities = arrayListOf()
         recycler.addItemDecoration(
-            DividerItemDecoration(context:this,DividerItemDecoration.VERTICAL)
+            DividerItemDecoration(
+                this,
+                DividerItemDecoration.VERTICAL
+            )
         )
-        )
-        mAdapter = CitiesAdapter(mCities, context:this){city ->
+        mAdapter = CitiesAdapter(mCities, this) { city ->
             cityOnClick(city)
-
         }
+
         recycler.adapter = mAdapter
     }
 
     private fun cityOnClick(city: Cities){
-        Log.d(TAG, msg:"Click on: $city")
+        Log.d(TAG, "Click on: $city")
     }
 
     private fun generateCities() {
-        val contactsString = readContactJsonFile()
+        val citiesString = readCityJsonFile()
         try {
-            val contactsJson = JSONArray(contactsString)
-            for (i in 0 until contactsJson.length()) {
-                val contactJson = contactsJson.getJSONObject(i)
-                val contact = Contact(
-                    contactJson.getString("first_name"),
-                    contactJson.getString("last_name"),
-                    contactJson.getString("email"),
-                    contactJson.getString("imageUrl")
+            val citiesJson = JSONArray(citiesString)
+            for (i in 0 until citiesJson.length()) {
+                val cityJson = citiesJson.getJSONObject(i)
+                val city = Cities(
+                    cityJson.getString("title"),
+                    cityJson.getString("description"),
+                    cityJson.getString("punctuation"),
+                    cityJson.getString("photoUrl")
                 )
-                Log.d(TAG, "generateContacts: $contact")
-                mContacts.add(contact)
+                Log.d(TAG, "generateCities: $city")
+                mCities.add(city)
             }
 
             mAdapter.notifyDataSetChanged()
         } catch (e: JSONException) {
             e.printStackTrace()
         }
+    }
+
+    private fun readCityJsonFile(): String? {
+        var citiesString: String? = null
+        try {
+            val inputStream = assets.open("cities.json")
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+
+            citiesString = String(buffer)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return citiesString
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
 
