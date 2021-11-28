81a79a19d788b92f794f864eee0df1f364d58df2
HacheCe <hectoremilcortes@gmail.com>
2021-11-27 23:57:55 -0500

    Sprint 3 - Lista de ciudades con fragment

---
+++ app/src/main/java/com/example/poi/ListFragment.kt
@@ -0,0 +1,123 @@
+package com.example.poi
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DividerItemDecoration
+import org.json.JSONArray
+import org.json.JSONException
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.util.ArrayList
+
+class ListFragment : Fragment() {
+    private lateinit var mCities: ArrayList<Cities>
+    private lateinit var mAdapter: CitiesAdapter
+    private lateinit var recycler: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recycler = view.findViewById(R.id.lstcities)
+        setupRecyclerView()
+        generateCities()
+    }
+
+    private fun setupRecyclerView(){
+        mCities = arrayListOf()
+        recycler.addItemDecoration(
+            DividerItemDecoration(
+                recycler.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        mAdapter = CitiesAdapter(mCities, recycler.context) { city ->
+            cityOnClick(city)
+        }
+
+        recycler.adapter = mAdapter
+    }
+
+    private fun cityOnClick(city: Cities){
+        Log.d(TAG, "Click on: $city")
+        city?.let {
+            navigateToDetail(it)
+        }
+    }
+
+    private fun navigateToDetail(city: Cities) {
+        Log.d(TAG, "Click on: $city")
+//        val intent = Intent {
+//            putExtra(KEY_CITYNAME, city.title)
+//            putExtra(KEY_CITYDESC, city.description)
+//            putExtra(KEY_CITYPOINTS, city.punctuation)
+//            putExtra(KEY_PHOTOURL, city.photoURL)
+//        }
+//
+//        startActivity(intent)
+    }
+
+    private fun generateCities() {
+        val citiesString = readCityJsonFile()
+        try {
+            val citiesJson = JSONArray(citiesString)
+            for (i in 0 until citiesJson.length()) {
+                val cityJson = citiesJson.getJSONObject(i)
+                val city = Cities(
+                    cityJson.getString("title"),
+                    cityJson.getString("description"),
+                    cityJson.getString("punctuation"),
+                    cityJson.getString("photoURL")
+                )
+                Log.d(TAG, "generateCities: $city")
+                mCities.add(city)
+            }
+
+            mAdapter.notifyDataSetChanged()
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun readCityJsonFile(): String? {
+        var citiesString: String? = null
+        try {
+            val inputStream = context?.assets?.open ("cities.json")
+            val size = inputStream?.available()
+            val buffer = size?.let { ByteArray(it) }
+            inputStream?.read(buffer)
+            inputStream?.close()
+
+            citiesString = buffer?.let { String(it) }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return citiesString
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        const val KEY_CITYNAME = "city_name"
+        const val KEY_CITYDESC = "city_desc"
+        const val KEY_CITYPOINTS = "city_points"
+        const val KEY_PHOTOURL = "city_photourl"
+    }
+}
--- app/src/main/java/com/example/poi/MainActivity.kt
+++ app/src/main/java/com/example/poi/MainActivity.kt
@@ -2,88 +2,20 @@ package com.example.poi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
-import org.json.JSONArray
-import org.json.JSONException
-import java.io.IOException
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mCities: ArrayList<Cities>
-    private lateinit var mAdapter: CitiesAdapter
-    private lateinit var recycler: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler = findViewById(R.id.lstcities)
-        setupRecyclerView()
-        generateCities()
-    }
+        //Create fragment
+        val fragment = ListFragment()
 
-    private fun setupRecyclerView(){
-        mCities = arrayListOf()
-        recycler.addItemDecoration(
-            DividerItemDecoration(
-                this,
-                DividerItemDecoration.VERTICAL
-            )
-        )
-        mAdapter = CitiesAdapter(mCities, this) { city ->
-            cityOnClick(city)
+        //Create transaction
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentcontainerlist,fragment)
+            commit()
         }
-
-        recycler.adapter = mAdapter
-    }
-
-    private fun cityOnClick(city: Cities){
-        Log.d(TAG, "Click on: $city")
-    }
-
-    private fun generateCities() {
-        val citiesString = readCityJsonFile()
-        try {
-            val citiesJson = JSONArray(citiesString)
-            for (i in 0 until citiesJson.length()) {
-                val cityJson = citiesJson.getJSONObject(i)
-                val city = Cities(
-                    cityJson.getString("title"),
-                    cityJson.getString("description"),
-                    cityJson.getString("punctuation"),
-                    cityJson.getString("photoURL")
-                )
-                Log.d(TAG, "generateCities: $city")
-                mCities.add(city)
-            }
-
-            mAdapter.notifyDataSetChanged()
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun readCityJsonFile(): String? {
-        var citiesString: String? = null
-        try {
-            val inputStream = assets.open("cities.json")
-            val size = inputStream.available()
-            val buffer = ByteArray(size)
-            inputStream.read(buffer)
-            inputStream.close()
-
-            citiesString = String(buffer)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        return citiesString
-    }
-
-    companion object {
-        private val TAG = MainActivity::class.java.simpleName
     }
 }
 
